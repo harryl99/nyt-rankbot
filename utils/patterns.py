d556e3d514cb668e2b0f04330080d441616e714a
Harry Luck <79798424+harryl99@users.noreply.github.com>
2024-02-16 10:40:07 +0000

    Fix connections

--- utils/patterns.py
+++ utils/patterns.py
@@ -6,6 +6,6 @@ This module defines regular expression patterns for extracting score info.
 
 import re
 
-connections_pattern = re.compile(r"Connections\nPuzzle #[0-9]+")
+connections_pattern = re.compile(r"Connections.*\nPuzzle #[0-9]+")
 mini_pattern = re.compile(r"badges\/games\/mini.+t=([0-9]+)")
 wordle_pattern = re.compile(r"Wordle [0-9]+ (.)/[0-9]")
