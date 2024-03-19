1d2d88f473bfb150f4692084e56db1c8b9169497
harryl99 <79798424+harryl99@users.noreply.github.com>
2024-03-19 12:03:52 +0000

    Update Worlde regex pattern to reflect going 1,000+

--- utils/patterns.py
+++ utils/patterns.py
@@ -10,4 +10,4 @@ connections_pattern = re.compile(r"Connections.*\nPuzzle #[0-9]+")
 mini_pattern = re.compile(r"badges\/games\/mini.+t=([0-9]+)")
 # Mini scores are sent differently if using the app
 mini_pattern_app = re.compile(r"I solved the .* New York Times Mini Crossword in (.+)!")
-wordle_pattern = re.compile(r"Wordle [0-9]+ (.)/[0-9]")
+wordle_pattern = re.compile(r"Wordle.+(.)/[0-9]")
